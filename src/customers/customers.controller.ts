f65b2851fbc4aa465d54de61b4c84c922451e547
Juan Ruiz <ruizuranojuanmanuel@gmail.com>
2022-08-17 03:09:38 +0100

    defined first endpoints

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+    @Get()
+    getCustomers(): string {
+        return "Return all the customers in the database"
+    }
+
+    @Get(':id')
+    getCustomer(@Param('id') id: string): string {
+        return "Return an specific customer"
+    }
+
+    @Post()
+    createCustomer(@Body() customer): string {
+        return "Create a new customer"
+    }
+
+    @Put(':id')
+    updateCustomer(@Param('id') id: string, @Body() customer): string {
+        return "Update an existing customer"
+    }
+
+    @Delete(':id')
+    deleteCustomer(@Param('id') id: string): string {
+        return "Delete an existing customer"
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    getUsers(): string {
+        return "Return all the users in the database"
+    }
+
+    @Get(':id')
+    getUser(@Param('id') id: string): string {
+        return "Return an specific user"
+    }
+
+    @Post()
+    createUser(@Body() user): string {
+        return "Create a new user"
+    }
+
+    @Put(':id')
+    updateUser(@Param('id') id: string, @Body() user): string {
+        return "Update an existing user"
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: string): string {
+        return "Delete an existing user"
+    }
+
+    //Need further info of the difference between updating a user and changing its admin status
+    @Put('admin/:id')
+    changeUserAdminStatus(@Param('id') id: string, @Body() user): string {
+        return "Update an existing user admin status"
+    }
+}
