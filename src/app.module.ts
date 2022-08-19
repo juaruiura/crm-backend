9457d2d1a36942cc059f9c419eed0ee0eedb09e9
Juan Ruiz <ruizuranojuanmanuel@gmail.com>
2022-08-19 02:23:43 +0100

    implement authentication: /auth/login returns an access token

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     CustomersModule,
     UsersModule,
+    AuthModule,
     MongooseModule.forRoot('mongodb://root:root@mongodb:27017/crm?serverSelectionTimeoutMS=2000&authSource=admin'),
   ],
   controllers: [AppController],
